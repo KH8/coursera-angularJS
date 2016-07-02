ef21eab9b1417ce47ea548438056e43971968e35
H8 <reczek.krzysztof@gmail.com>
2016-07-02 22:58:38 +0200

    w2e1.1: done

--- w2.1/Gruntfile.js
+++ w2.1/Gruntfile.js
@@ -2,7 +2,9 @@
 
 module.exports = function (grunt) {
     require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -54,14 +56,117 @@ module.exports = function (grunt) {
                     'dist/'
                 ]
             }
+        },
+        useminPrepare: {
+            html: 'app/*.html',
+            options: {
+                dest: 'dist'
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {}
+        },
+        uglify: {
+            dist: {}
+        },
+        cssmin: {
+            dist: {}
+        },
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 10
+            },
+            release: {
+                files: [{
+                    src: [
+                        'dist/scripts/*.js',
+                        'dist/styles/*.css'
+                    ]
+                }]
+            }
+        },
+        usemin: {
+            html: 'dist/*.html',
+            css: ['dist/styles/*.css'],
+            options: {
+                assetsDirs: ['dist', 'dist/styles']
+            }
+        },
+        watch: {
+            copy: {
+                files: [
+                    'app/**',
+                    '!app/**/*.css',
+                    '!app/**/*.js'
+                ],
+                tasks: [ 'build' ]
+            },
+            scripts: {
+                files: [
+                    'app/scripts/app.js'
+                ],
+                tasks: [ 'build' ]
+            },
+            styles: {
+                files: [
+                    'app/styles/mystyles.css'
+                ],
+                tasks: [ 'build' ]
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    'app/{,*/}*.html',
+                    '.tmp/styles/{,*/}*.css',
+                    'app/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            dist: {
+                options: {
+                    open: true,
+                    base: {
+                        path: 'dist',
+                        options: {
+                            index: 'menu.html',
+                            maxAge: 300000
+                        }
+                    }
+                }
+            }
         }
     });
 
     grunt.registerTask('build', [
         'clean',
         'jshint',
-        'copy'
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'copy',
+        'filerev',
+        'usemin'
     ]);
+
+    grunt.registerTask('serve', [
+        'build',
+        'connect:dist',
+        'watch']
+    );
 
     grunt.registerTask('default', [
         'build'
