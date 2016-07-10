cb8113ff84f3b359eec92a03a94ccb2ed81b5c15
H8 <reczek.krzysztof@gmail.com>
2016-07-10 21:07:39 +0200

    w2e3.1: done

--- w2.3.1/app/scripts/app.js
+++ w2.3.1/app/scripts/app.js
@@ -1,18 +1,42 @@
 'use strict';
 
-var app = angular.module('confusionApp', []);
-app.controller('ContactController', ['$scope', function ($scope) {
-    $scope.feedback = {
-        channel: "",
-        fistName: "",
+function getPristineFeedback() {
+    return {
+        myChannel: "",
+        firstName: "",
         lastName: "",
         agree: false,
         email: ""
     };
+}
+
+var app = angular.module('confusionApp', []);
+app.controller('ContactController', ['$scope', function ($scope) {
+    $scope.feedback = getPristineFeedback();
+    $scope.invalidChannelSelection = false;
 }]);
 
 app.controller('FeedbackController', ['$scope', function ($scope) {
+    $scope.channels = [
+        {
+            value: "tel", 
+            label: "Tel."
+        },
+        {
+            value: "Email",
+            label: "Email"
+        }
+    ];
     $scope.sendFeedback = function () {
-        //TODO!!!
+        console.log($scope.feedback);
+        if ($scope.feedback.agree && !$scope.feedback.myChannel) {
+            $scope.invalidChannelSelection = true;
+            console.log('incorrect');
+        } else {
+            $scope.invalidChannelSelection = false;
+            $scope.feedback = getPristineFeedback();
+            $scope.feedbackForm.$setPristine();
+            console.log($scope.feedback);
+        }
     };
 }]);
