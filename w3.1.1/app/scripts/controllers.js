8b97dc4b02ffa499e59d94bc5223daadd033ad30
H8 <reczek.krzysztof@gmail.com>
2016-07-17 12:44:18 +0200

    w3e1.1: done

--- w3.1.1/app/scripts/controllers.js
+++ w3.1.1/app/scripts/controllers.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-var options = ['', 'appetizer', 'mains', 'dessert'];
+var options = ["", "appetizer", "mains", "dessert"];
 
-var app = angular.module('confusionApp', []);
-app.controller('MenuController', ['$scope', 'menuFactory', function ($scope, menuFactory) {
+var app = angular.module("confusionApp", []);
+app.controller("MenuController", ["$scope", "dishesService", function ($scope, dishesService) {
     $scope.tab = 0;
     $scope.filterText = options[$scope.tab];
     $scope.showDetails = false;
 
-    $scope.dishes = menuFactory.getDishes();
+    $scope.dishes = dishesService.getDishes();
 
     $scope.select = function (index) {
         $scope.tab = index;
@@ -24,8 +24,8 @@ app.controller('MenuController', ['$scope', 'menuFactory', function ($scope, men
     };
 }]);
 
-app.controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
-    $scope.dish = menuFactory.getDish(3);
+app.controller("DishDetailController", ["$scope", "dishesService", function($scope, dishesService) {
+    $scope.dish = dishesService.getDish(3);
     $scope.range = function(n) {
         return new Array(n);
     };
