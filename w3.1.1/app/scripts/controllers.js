40af2954a7cccec4539ecc634a06038fc6248f8b
H8 <reczek.krzysztof@gmail.com>
2016-07-17 12:33:18 +0200

    w3e1.1: in progress

--- w3.1.1/app/scripts/controllers.js
+++ w3.1.1/app/scripts/controllers.js
@@ -3,50 +3,12 @@
 var options = ['', 'appetizer', 'mains', 'dessert'];
 
 var app = angular.module('confusionApp', []);
-app.controller('MenuController', ['$scope', function ($scope) {
+app.controller('MenuController', ['$scope', 'menuFactory', function ($scope, menuFactory) {
     $scope.tab = 0;
     $scope.filterText = options[$scope.tab];
     $scope.showDetails = false;
 
-    var dishes = [
-        {
-            name:'Uthapizza',
-            image: 'images/uthapizza.png',
-            category: 'mains',
-            label:'Hot',
-            price:'4.99',
-            description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-            comment: ''
-        },
-        {
-            name:'Zucchipakoda',
-            image: 'images/zucchipakoda.png',
-            category: 'appetizer',
-            label:'',
-            price:'1.99',
-            description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-            comment: ''
-        },
-        {
-            name:'Vadonut',
-            image: 'images/vadonut.png',
-            category: 'appetizer',
-            label:'New',
-            price:'1.99',
-            description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-            comment: ''
-        },
-        {
-            name:'ElaiCheese Cake',
-            image: 'images/elaicheesecake.png',
-            category: 'dessert',
-            label:'',
-            price:'2.99',
-            description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-            comment: ''
-        }
-    ];
-    $scope.dishes = dishes;
+    $scope.dishes = menuFactory.getDishes();
 
     $scope.select = function (index) {
         $scope.tab = index;
@@ -60,4 +22,12 @@ app.controller('MenuController', ['$scope', function ($scope) {
     $scope.toggleShowDetails = function () {
         $scope.showDetails = !$scope.showDetails;
     };
+}]);
+
+app.controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+    $scope.dish = menuFactory.getDish(3);
+    $scope.range = function(n) {
+        return new Array(n);
+    };
+    $scope.orderBy = "rating";
 }]);
